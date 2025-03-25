1852955009cf5b9e547605b460520842c9abbf05
Bispo1307 <alanoliveirab09@gmail.com>
2025-03-25 18:53:13 +0000

    Commit Projeto Finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,45 +3,45 @@
 int main() {
         int população;
         printf ("Digite a População - Carta 1:\n");
-        scanf("%d", população);
+        scanf("%d", &população);
         printf ("A população é de: %d\n");
         
         float área;
         printf ("Digite a Área - Carta 1:\n");
-        scanf("%f", área);
+        scanf("%f", &área);
         printf ("A área é de: %f\n");
         
         float PIB;
         printf ("Digite o PIB - Carta 1:\n");
-        scanf("%f", PIB);
+        scanf("%f", &PIB);
         printf ("O PIB é de: %f\n");
         
         int número_de_pontos_turísticos;
         printf ("Digite o número de pontos turísticos - Carta 1:\n");
-        scanf("%d", população);
-        printf ("O número de pontos turísticos é de: %d\n");
+        scanf("%d", &número_de_pontos_turísticos);
+        printf ("O Número de Pontos Turísticos é de: %d\n");
      return 0;
     }
 
 int main() {
         int população;
         printf ("Digite a População - Carta 2:\n");
-        scanf("%d", população);
+        scanf("%d", &população);
         printf ("A população é de: %d\n");
         
         float área;
         printf ("Digite a Área - Carta 2:\n");
-        scanf("%f", área);
+        scanf("%f", &área);
         printf ("A área é de: %f\n");
         
         float PIB;
         printf ("Digite o PIB - Carta 2:\n");
-        scanf("%f", PIB);
+        scanf("%f", &PIB);
         printf ("O PIB é de: %f\n");
         
         int número_de_pontos_turísticos;
         printf ("Digite o número de pontos turísticos - Carta 2:\n");
-        scanf("%d", população);
+        scanf("%d", &número_de_pontos_turísticos);
         printf ("O número de pontos turísticos é de: %d\n");
      return 0;
     }
